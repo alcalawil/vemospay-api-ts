703167d70a8503d4523ad1e12e8a9fc9542699e9
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-10-22 20:40:26 -0400

    fix config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const env = process.env;
 
-if (!env.DB_URL) {
-	throw new Error('DB_URL not set');
-}
+// if (!env.DB_URL) {
+// 	throw new Error('DB_URL not set');
+// }
 
-if (!env.JWT_SECRET_KEY) {
-	throw new Error('JWT_SECRET_KEY not set');
-}
+// if (!env.JWT_SECRET_KEY) {
+// 	throw new Error('JWT_SECRET_KEY not set');
+// }
 
 export type Config = {
 	server: {
@@ -31,6 +31,6 @@ export const config: Config = {
 		url: env.DB_URL || 'mongodb://localhost:27017/myDb'
 	},
 	auth: {
-		secretKey: env.JWT_SECRET_KEY
+		secretKey: env.JWT_SECRET_KEY || 'secretKey'
 	}
 };
